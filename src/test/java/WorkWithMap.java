bb33fa6f43a9b38c764a15587ea8ee170fe26566
Jelena Gorfunga <jelena.gorfunga@inbox.lv>
2020-08-10 17:01:25 +0300

    initial

---
+++ src/test/java/WorkWithMap.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import java.awt.geom.Point2D;
+
+public class WorkWithDistance {
+
+    @Test
+    public void distanceBetweenTwoPoints() {
+        double x1 = 1;
+        double y1 = 2;
+        double x2 = 3;
+        double y2 = 4;
+
+        double distance = getDistance(x1, x2, y1, y2);
+        System.out.println("Расстояние между двумя точками - " + distance + ".");
+
+            }
+    private double getDistance(double x1, double x2, double y1, double y2) {
+        return Point2D.distance(x1,y1,x2,y2);
+    }
+}
