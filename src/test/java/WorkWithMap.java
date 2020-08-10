d9115bc1a734dcf488ef5473acd6102192b2d9b3
Jelena Gorfunga <jelena.gorfunga@inbox.lv>
2020-08-10 17:12:25 +0300

    initial

--- src/test/java/WorkWithMap.java
+++ src/test/java/WorkWithMap.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import java.awt.geom.Point2D;
+import java.text.DecimalFormat;
 
 public class WorkWithMap {
 
@@ -10,8 +11,10 @@ public class WorkWithMap {
         double x2 = 3;
         double y2 = 4;
 
+        DecimalFormat df = new DecimalFormat("###.###");
+
         double distance = getDistance(x1, x2, y1, y2);
-        System.out.println("Расстояние между двумя точками - " + distance + ".");
+        System.out.println("Расстояние между двумя точками - " + df.format(distance) + ".");
 
             }
     private double getDistance(double x1, double x2, double y1, double y2) {
