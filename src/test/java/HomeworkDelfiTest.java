bb8054ddb601d796a382cb6b4800eaf5f2b20528
Jelena Gorfunga <jelena.gorfunga@inbox.lv>
2020-08-10 17:33:42 +0300

    initial

--- src/test/java/HomeworkDelfiTest.java
+++ src/test/java/HomeworkDelfiTest.java
@@ -4,16 +4,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
 
 public class HomeworkDelfiTest {
 
     private WebDriver driver;
     private final String EXPECTED_TITLE = "В Латвии за сутки выявлено три новых случая Covid-19";
     private final int EXPECTED_COMMENT_COUNT = 15;
+
+    private final By FIRST_TITLE = By.xpath(".//h1[contains(@class, 'headline__title')]");
+    private final By COMMENTS_COUNT = By.xpath(".//a[contains(@class, 'comment-count')]");
 
     @BeforeEach
     public void start() {
@@ -25,18 +25,25 @@ public class HomeworkDelfiTest {
 
     @Test
     public void homeDelfiTestTitle() {
-        String actualTitle = driver.findElement(By.xpath(".//h1[contains(@class, 'headline__title')]")).getText();
+        String actualTitle = driver.findElement(FIRST_TITLE).getText();
         System.out.println("Первый заголовок: " + actualTitle);
         Assertions.assertEquals(EXPECTED_TITLE, actualTitle, "First title is not correct!");
     }
 
     @Test
     public void homeDelfiTestCommentCount() {
-        String commentsCount = driver.findElement(By.xpath(".//a[contains(@class, 'comment-count')]")).getText();
-        String commentsCountWithoutSymbols = commentsCount.substring(1, commentsCount.length() - 1);
-        System.out.println("Количество комментариев у первого заголовка: " + commentsCount);
-        int actualCommentCount = Integer.valueOf(commentsCountWithoutSymbols);
+        int actualCommentCount = 0;
+        if (!driver.findElements(COMMENTS_COUNT).isEmpty()) {
+            actualCommentCount = parseCommentCount(driver.findElement(COMMENTS_COUNT).getText());
+        }
+        System.out.println("Количество комментариев к первому заголовку: " + actualCommentCount);
         Assertions.assertEquals(EXPECTED_COMMENT_COUNT, actualCommentCount, "Comment count is not correct!");
+
+    }
+
+    private int parseCommentCount(String textToParse) {
+        textToParse = textToParse.substring(1, textToParse.length() - 1);
+        return Integer.parseInt(textToParse);
     }
 
     @AfterEach
